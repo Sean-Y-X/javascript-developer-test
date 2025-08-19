3fce6dd16fb46690bd4cb2cb4e42eb57930332ac
sean xiao <zy05530@gmail.com>
2025-08-19 11:04:16 +1000

    get arnie quotes from mock urls

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,29 @@
 const { httpGet } = require('./mock-http-interface');
 
+const SUCCESS_STATUS = 200;
+const QUOTE_KEY = Object.freeze({
+  ARNIE_QUOTE: 'Arnie Quote',
+  FAILURE: 'FAILURE',
+});
+
+const fetchArnieQuote = async (url) => {
+  try {
+    const { status, body } = await httpGet(url);
+    const message = JSON.parse(body).message;
+
+    return status === SUCCESS_STATUS
+      ? { [QUOTE_KEY.ARNIE_QUOTE]: message }
+      : { [QUOTE_KEY.FAILURE]: message };
+  } catch (error) {
+    console.error(`Failed to fetch quote from ${url}`, error);
+    return { [QUOTE_KEY.FAILURE]: `Failed to fetch quote` };
+  }
+};
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const promises = urls.map(fetchArnieQuote);
+
+  return Promise.all(promises);
 };
 
 module.exports = {
